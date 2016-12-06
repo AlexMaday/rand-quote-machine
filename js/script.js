b3f07d31e36cf3ced583c394f4d8799b83159ed9
Alex Maday <alex.maday@gmail.com>
2016-12-05 20:01:25 -0600

    update all image links urls

--- js/script.js
+++ js/script.js
@@ -3,77 +3,77 @@ var quotations =
    {
       "text":"The best way to predict the future is to design it.",
       "author":"Buckminster Fuller",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/00.jpg"
+      "authorImage": "./images/00.jpg"
    },
    {
       "text":"Anyone who has never made a mistake, has never tried anything new.",
       "author":"Albert Einstein",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/01.jpg"
+      "authorImage": "./images/01.jpg"
    },
    {
       "text":"Brevity is the soul of wit.",
       "author":"William Shakespeare",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/02.jpg"
+      "authorImage": "./images/02.jpg"
    },
    {
       "text":"Originality is nothing but judicious imitation.",
       "author":"Voltaire",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/03.jpg"
+      "authorImage": "./images/03.jpg"
    },
    {
       "text":"Everything should be made as simple as possible, but not simpler.",
       "author":"Albert Einstein",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/01.jpg"
+      "authorImage": "./images/01.jpg"
    },
    {
       "text":"I have not failed, I've just found 10,000 ways that won't work.",
       "author":"Thomas Edison",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/04.jpg"
+      "authorImage": "./images/04.jpg"
    },
    {
       "text":"The unexamined life is not worth living.",
       "author":"Socrates",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/05.jpg"
+      "authorImage": "./images/05.jpg"
    },
    {
       "text":"An essential element of any art is risk. If you don't take a risk then how are you going to make something really beautiful, that hasn't been seen before",
       "author":"Francis Ford Coppola",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/06.jpg"
+      "authorImage": "./images/06.jpg"
    },
    {
       "text":"If you tell the truth, you don't have to remember anything",
       "author":"Mark Twain",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/07.jpg"
+      "authorImage": "./images/07.jpg"
    },
    {
       "text":"Always remember that you are absolutely unique. Just like everyone else.",
       "author":"Margaret Mead",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/08.jpg"
+      "authorImage": "./images/08.jpg"
    },
    {
        "text":"The only way to have a friend is to be one.",
       "author":"Ralph Waldo Emerson",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/09.jpg"
+      "authorImage": "./images/09.jpg"
    },
    {
       "text": "Ah, well, I am a great and sublime fool. But then I am God's fool, and all His works must be contemplated with respect.",
       "author": "Mark Twain",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/07.jpg"
+      "authorImage": "./images/07.jpg"
    },
    {
       "text": "Be the change that you wish to see in the world.",
       "author": "Mahatma Gandhi",
-      "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/10.jpg"
+      "authorImage": "./images/10.jpg"
    },
    {
      "text": "They who can give up essential liberty to obtain a little temporary safety deserve neither liberty nor safety",
      "author": "Benjamin Franklin",
-     "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/11.jpg"
+     "authorImage": "./images/11.jpg"
    },
    {
        "text": "Everyone should learn how to program a computer because it teaches you how to think.",
        "author": "Steve Jobs",
-       "authorImage": "https://dl.dropboxusercontent.com/u/5729928/images/rqm/12.jpg"
+       "authorImage": "./images/12.jpg"
    }
 ];
 var quoteText = document.getElementById('quoteText');
